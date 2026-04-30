5331dc5e6963b361560ac1580b12618249991fc4
Khalid Afghanmal <askkhalid6@gmail.com>
2026-04-30 09:21:15 +0430

    Optimize images loading

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/faq" element={<Navigate to="/#faq" replace />} />
         <Route path="/contact" element={<Navigate to="/#contact" replace />} />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} /> 
       </Route> 
     </Routes>
   )
