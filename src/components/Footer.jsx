6b21304df556cc05abcd04c6b30654fc643abae7
Khalid Afghanmal <askkhalid6@gmail.com>
2026-04-30 09:21:11 +0430

    Fix minor UI alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   useEffect(() => {
     // keep year correct in long-lived sessions
-    const t = window.setInterval(() => setYear(new Date().getFullYear()), 60_000) 
+    const t = window.setInterval(() => setYear(new Date().getFullYear()), 60_000)
     return () => window.clearInterval(t)
   }, [])
 
